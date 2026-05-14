1a3b744273535e0aa010c443d1bf492edeb6d6dc
JaeyXu <xjy230702@mail.ustc.edu.cn>
2026-05-14 14:44:41 +0800

    Update config.py

--- Improvement/Model_and_Domain_imp/CNN-Mamba/subjective_adaptive/2s/config.py
+++ Improvement/Model_and_Domain_imp/CNN-Mamba/subjective_adaptive/2s/config.py
@@ -1,10 +1,8 @@
 import torch
 
 # models
-process_data_dir = '/disk1/jyxu/DaChuang/REFER_CODE'
-result_dir='/disk1/jyxu/DaChuang/Improvement/Model_and_Domain_imp/mamba/subjective_adaptive/2s'
-pretrain_model_dir='/disk1/jyxu/DaChuang/Improvement/Model_and_Domain_imp/mamba/subjective_adaptive/2s/pretrain_model'
-finetune_model_dir='/disk1/jyxu/DaChuang/Improvement/Model_and_Domain_imp/mamba/subjective_adaptive/2s/finetune_model'
+pretrain_model_dir='./pretrain_model'
+finetune_model_dir='./finetune_model'
 dataset_name = 'KUL3_1D.mat'
 
 
